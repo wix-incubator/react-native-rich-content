6550e4676cd28b2dbac7069f805a26a94da1b208
Guy Haviv <guyhav@wix.com>
2021-12-12 10:57:45 +0200

    Demo: * Created basic screens (Home, Editor) * Configured navigation

---
+++ demo/src/screens/HomeScreen.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {RootStackParamList} from './RootStackParamList';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+interface HomeScreenProps {}
+
+type HomeScreenType = HomeScreenProps & NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const HomeScreen: React.FC<HomeScreenType> = (props) => {
+    const {navigation} = props;
+
+    return (  
+    <View style={styles.container}>
+        <Text>Welcome to the Rich Content demo app</Text>
+        <Pressable onPress={() => navigation.navigate('Editor')}>
+          <Text>Open Editor</Text>
+        </Pressable>
+      </View>
+    )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
