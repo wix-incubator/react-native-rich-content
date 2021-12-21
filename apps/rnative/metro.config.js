981954271247b8b385338a122e59bf4563034b03
Guy Haviv <guyhav@wix.com>
2021-12-21 12:26:02 +0200

    * Restructure repo for yarn workspaces * Add demo app with webview support * initialize editor package

---
+++ apps/rnative/metro.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const getWorkspaces = require('get-yarn-workspaces');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const appJson = require('./package.json');
+
+const rootPath = path.resolve(__dirname, '../../');
+const workspaces = getWorkspaces(rootPath);
+const installedDependencies = appJson.dependencies;
+
+const extraNodeModules = {};
+Object.keys(installedDependencies).forEach(dep => {
+  extraNodeModules[dep] = path.resolve(__dirname, 'node_modules', dep);
+});
+Object.keys(appJson.devDependencies).forEach(dep => {
+  extraNodeModules[dep] = path.resolve(__dirname, 'node_modules', dep);
+});
+
+module.exports = {
+  projectRoot: path.resolve(rootPath, 'apps/rnative'),
+
+  watchFolders: [path.resolve(__dirname, 'node_modules'), ...workspaces],
+
+  resolver: {
+    blacklistRE: exclusionList(
+      workspaces.map(
+        workspacePath =>
+          `/${workspacePath.replace(
+            /\//g,
+            '[/\\\\]',
+          )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`,
+      ),
+    ),
+    extraNodeModules,
+  },
+  resetCache: true,
+};
---
+++ packages/editor/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+export const Editor = (props) => {
+    return (
+        <View>
+            <Text>Hello to Yuval</Text>
+            <WebView source={{uri: 'https://www.wix.com'}} style={{flex: 0, width: 200, height: 200}}/>
+        </View>
+    )
+}
